0f549492134785fd9e8d50242058c8a3dd908777
Aye Chan Wai <ayechanwai.edu@gmail.com>
2024-01-17 21:33:07 +0630

    Sending data fragment to activity

---
+++ app/src/main/java/com/techmania/sendingdatafromfragmenttoactivity/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.techmania.sendingdatafromfragmenttoactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
+class MainActivity : AppCompatActivity() {
+    lateinit var name : TextView
+    lateinit var email : TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        name = findViewById(R.id.textViewName)
+        email = findViewById(R.id.textViewEmail)
+
+        val fm : FragmentManager = supportFragmentManager
+        val ft : FragmentTransaction = fm.beginTransaction()
+        val myFragment = MyFragment()
+        ft.add(R.id.frame,myFragment)
+        ft.commit()
+    }
+    fun takeData(userName : String , userEmail : String){
+        name.text= userName
+        email.text= userEmail
+    }
+}
